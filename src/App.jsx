5cf447d55cb4d6b5f1425d1deb3e0fc993b196d1
Anuragpandey26 <anuragkumarpandey2001@gmail.com>
2024-03-13 23:40:57 +0530

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import CryptoDetail from './pages/CryptoDetail';
 import CryptoHome from "./pages/CryptoHome";
 import Navbar from "./component/navbar"
 function App() {
 
   return (
-    <BrowserRouter>
-      <Navbar />
-      <CryptoHome />
+    <>
+    <Navbar />
       <Routes>
         <Route path="/" element={<CryptoHome />} />
         <Route path="/coin/:id" element={<CryptoDetail />} />
       </Routes>
-    </BrowserRouter>
+    </>
   )
 }
 
