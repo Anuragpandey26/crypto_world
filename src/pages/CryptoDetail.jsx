5dcd9f56177ef738bb7d4cc44d9e55d0a88305e9
Anuragpandey26 <anuragkumarpandey2001@gmail.com>
2024-03-13 22:18:39 +0530

    initial commit

---
+++ src/pages/CryptoDetail.jsx
@@ -0,0 +1,12 @@
+import CoinDetail from "../component/CoinDetail"
+import HistoryChart from "../component/HistoryChart"
+const CryptoDetail = () => {
+  return (
+    <>
+    <HistoryChart />
+   <CoinDetail/>
+   </>
+  )
+}
+
+export default CryptoDetail
---
+++ src/pages/CryptoHome.jsx
@@ -0,0 +1,15 @@
+
+import Trending from "../component/Trending"
+import Markets from "../component/market"
+
+
+const CryptoHome = () => {
+  return (
+    <div className="wrapper-container">
+      <Trending />
+      <Markets />
+    </div>
+  )
+}
+
+export default CryptoHome
